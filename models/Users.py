11b4d34bdf98e475dff4a28f271c8244baf0cd4f
Asım Kaymak <kaymakasm@gmail.com>
2023-09-20 20:16:02 +0300

    feat: create users model

---
+++ models/Users.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
