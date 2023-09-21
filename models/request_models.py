ec788aebc4549e60036b615a2be9627332e2fd77
Asım Kaymak <kaymakasm@gmail.com>
2023-09-21 03:29:41 +0300

    feat: update request models

--- models/request_models.py
+++ models/request_models.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
 class UserLogin(BaseModel):
     username: str
@@ -8,3 +10,23 @@ class CustomerCreate(BaseModel):
     name: str
     surname: str
     gsm: str
+    birthDay: Optional[datetime] = None
+    gender: Optional[str] = None
+
+class CustomerUpdate(BaseModel):
+    name: Optional[str] = None
+    surname: Optional[str] = None
+    gsm: Optional[str] = None
+    birthDay: Optional[datetime] = None
+    gender: Optional[str] = None
+
+class PurchaseCreate(BaseModel):
+    customerId: int
+    purchaseDate: datetime
+    listingPrice: float
+    salePrice: float
+
+class DateRange(BaseModel):
+    beginningDate: datetime
+    endingDate: datetime
+
